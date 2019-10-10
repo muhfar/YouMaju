599dcb78f0110b58d3ea41bc52d940e147fbcb05
Anggreini Intan Permata Sari <anggreiniintn@gmail.com>
2019-10-10 15:04:20 +0700

    upload tentang fitur
    youkonek, youbel, youber

---
+++ resources/views/youbelajar.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>You &mdash; Colorlib Website Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
+    <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
+
+    <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <header class="site-navbar py-3" role="banner">
+
+      <div class="container gambaricon">
+        <div class="align-items-center" data-aos="fade-up" data-aos-delay="400" >
+         <a href="#"> <img src="{{asset('images/fav_icon_you_maju_YB.png')}}">
+                  
+    </header>
+   
+    <div class="site-section">
+      <div class="container">   
+
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="mb-0" style="color:#3c3c32">YouBelajar</h2>
+            <p class="color-black-opacity-5" style="color:#343a40">
+            Bagaimana tahapan belajar jadi youtuber terkenal ? Disini tempatnya !</p>
+          </div>
+        </div>
+
+        <div style="color:#343a40; font-size:20px;" >
+                <p align="justify">Setiap orang tentunya sudah tidak asing lagi dengan youtube. Youtube merupakan salah satu media yang 
+                    sangat dicari dan digemari oleh setiap orang. Banyak yang beranggapan bahwa youtube adalah salah satu media penghasil 
+                    uang tercepat dan termudah, namun youtuber sendiri tidak mengetahui bagaimana cara mendapatkan dan mengoptimalkan 
+                    penggunaan youtube. Terdapat beberapa tahapan yang harus diperhatikan guna menjadi youtuber profesional yakni diantaranya :
+           <ul> 1.	Pastikan anda sudah memiliki akun youtube chanel.</ul>
+           <ul> 2.	Jika anda sudah memiliki akun youtube, lakukan monetisasi pada akun youtube anda.</ul>
+           <ul> 3.	Sambungkan akun youtube anda ke AdSense.</ul>
+           <ul> 4.	Lakukan Conversion Rate ke Product Page.</ul>
+           <ul> 5.	Kemudian anda juga harus memperhatikan isi konten youtube chanel yang anda buat.</ul>
+                    Untuk lebih lengkapnya lagi mengenai tips, trik dan video tutorialnya cari di situs Youmaju, pada YouMaju berisikan 
+                    tahapan-tahapan menjadikan youtuber profesional secara lengkap.</p>
+            </div>
+        </div>
+    </div> 
+    
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="row">
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Tentang YouMaju</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">Tentang Kami</a></li>
+                  <li><a href="#">Bantuan</a></li>
+                  <li><a href="#">Kontak Kami</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Follow Us</h2>
+                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <h2 class="footer-heading mb-4"></h2>
+            
+          </div>
+        </div>
+          
+        </div>
+      </div>
+    </footer>
+  
+
+  <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+  <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
+  <script src="{{asset('js/jquery-ui.js')}}"></script>
+  <script src="{{asset('js/popper.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
+  <script src="{{asset('js/jquery.countdown.min.js')}}"></script>
+  <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap-datepicker.min.js')}}"></script>
+  <script src="{{asset('js/aos.js')}}" ></script>
+
+  <script src="{{asset('js/main.js')}}"></script>
+    
+  </body>
+</html>
---
+++ resources/views/youberbagi.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>You &mdash; Colorlib Website Template</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
+    <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
+
+    <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    
+  </head>
+  <body>
+  
+  <div class="site-wrap">
+
+    <header class="site-navbar py-3" role="banner">
+
+      <div class="container gambaricon">
+        <div class="align-items-center" data-aos="fade-up" data-aos-delay="400" >
+         <a href="#"> <img src="{{asset('images/fav_icon_you_maju_YB.png')}}">
+                  
+    </header>
+   
+    <div class="site-section">
+      <div class="container">   
+
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center border-primary">
+            <h2 class="mb-0" style="color:#3c3c32">YouBerbagi</h2>
+            <p class="color-black-opacity-5" style="color:#343a40">
+            Mau berbagi cerita dengan mudah, cepat,  praktis dan menguntungkan?</p>
+          </div>
+        </div>
+
+        <div style="color:#343a40; font-size:20px;" >
+                <p align="justify">Berbagi cerita adalah hal termudah yang dapat dilakukan setiap orang, setelah anda mendapatkan 
+                    pemebelajaran menjadi youtuber profesional tentunya anda haruslah mengetahui cara berbagi cerita terbaik yang anda 
+                    buat dan juga media pendukung lainnya yang membantu penjualan media promosi cerita anda. YouMaju adalah salah satu 
+                    media berbagi cerita terbaik yeng diberikan, karena dengan YouMaju didapatkan kemudahan, kecepatan dan keuntungan 
+                    yang berlimpah. YouMaju menawarkan berbagi cerita pada akun youtube chanel anda yang menguntungkan baik dari yotuber 
+                    maupun viewer. Pada YouMaju viewer memiliki keuntungan berupa point dari setiap chanel youtube yang mereka tonton.
+                     Point tersebut dapat ditukarkan berupa penawaran terbaik yang diberiakan YouMaju. Youtuber juga memperoleh kecepatan 
+                     pertambahan viewers karena dengan YouMaju chanel yang anda tawarkan menjadi lebih menarik untuk ditonton.</p>
+            </div>
+        </div>
+    </div> 
+    
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-9">
+            <div class="row">
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Tentang YouMaju</h2>
+                <ul class="list-unstyled">
+                  <li><a href="#">Tentang Kami</a></li>
+                  <li><a href="#">Bantuan</a></li>
+                  <li><a href="#">Kontak Kami</a></li>
+                  </ul>
+              </div>
+              <div class="col-md-3">
+                <h2 class="footer-heading mb-4">Follow Us</h2>
+                <a href="#" class="pl-0 pr-3"><span class="icon-facebook"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-twitter"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+                <a href="#" class="pl-3 pr-3"><span class="icon-linkedin"></span></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <h2 class="footer-heading mb-4"></h2>
+            
+          </div>
+        </div>
+          
+        </div>
+      </div>
+    </footer>
+  
+
+  <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+  <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
+  <script src="{{asset('js/jquery-ui.js')}}"></script>
+  <script src="{{asset('js/popper.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.min.js')}}"></script>
+  <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+  <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
+  <script src="{{asset('js/jquery.countdown.min.js')}}"></script>
+  <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap-datepicker.min.js')}}"></script>
+  <script src="{{asset('js/aos.js')}}" ></script>
+
+  <script src="{{asset('js/main.js')}}"></script>
+    
+  </body>
+</html>
