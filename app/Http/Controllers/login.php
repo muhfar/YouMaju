ee4a236c2832eed3b2309b63bc9bbf804f8cd085
muhfar <36448231+muhfar@users.noreply.github.com>
2019-10-21 20:25:47 +0700

    login google
    -return data

--- app/Http/Controllers/login.php
+++ app/Http/Controllers/login.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Socialite;
-use App\Services\SocialGoogleAccountService;
+use Illuminate\Support\Arr;
 
 
 class login extends Controller
@@ -12,13 +12,39 @@ class login extends Controller
 
 	public function redirect()
     {
+        // $client = new Google_Client();
+        // $client->setAuthConfig('.env');
+        // $client->addScope(GOOGLE_SERVICE_YOUTUBE::YOUTUBE_FORCE_SSL);
+        // $client->setRedirectUri('http://'. $_SERVER['HTTP_HOST'] . ('/callback'));
+
+        // $client->setAccessType('offline');
+        // $client->setApprovalPrompt('consent');
+
+        // $auth_url = $client->createAuthUrl();
+
         return Socialite::driver('google')->redirect();
     }
 
-	public function callback(SocialGoogleAccountService $service)
+	public function callback()
     {
-        //$user = $service->createOrGetUser(Socialite::driver('google')->user());
-        return redirect()->to('/');
+        $user = Socialite::driver('google')->user();
+
+        $user = json_decode(json_encode($user), true);
+
+        //data login from google
+        // if email not found in db save it
+        // else set session
+        $data = array(
+        	'google_id' => $user['id'],
+			'nama' => $user['name'] ,
+			'email'	 => $user['email'] ,
+			'token'	 => $user['token'] ,
+			'refresh_token'	 => $user['refreshToken'] ,
+			'photo' => $user['avatar']
+		);
+
+        // print_r($data);
+        return redirect('/profile');
     }
 
 }
