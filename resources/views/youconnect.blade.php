75e1254559bb3dd23da23815c0498e36ccdbb1cd
abdurrasyidhabib <[habib.rasyid11@gmail.com]>
2019-10-22 17:38:27 +0700

    ubah subjudul

--- resources/views/youconnect.blade.php
+++ resources/views/youconnect.blade.php
@@ -1,64 +1,66 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>YouMaju &mdash; YouConnect</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
-    <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}">
+<head>
+  <title>YouMaju &mdash; YouConnect</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
-    <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
-    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700">
+  <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
+  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+  <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
+  <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
 
-    <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
+  <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+  <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    
-  </head>
-  <body>
-  
+  <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+</head>
+
+<body>
+
   <div class="site-wrap">
 
     <header class="site-navbar py-3" role="banner">
 
       <div class="container gambaricon">
-        <div class="align-items-center" data-aos="fade-up" data-aos-delay="400" >
-         <a href="#"> <img src="{{asset('images/fav_icon_you_maju_YB.png')}}">
-                  
+        <div class="align-items-center" data-aos="fade-up" data-aos-delay="400">
+          <a href="#"> <img src="{{asset('images/fav_icon_you_maju_YB.png')}}">
+
     </header>
-   
+
     <div class="site-section">
-      <div class="container">   
+      <div class="container">
 
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center border-primary">
             <h2 class="mb-0" style="color:#3c3c32">YouConnect</h2>
             <p class="color-black-opacity-5" style="color:#343a40">
-            Mau tahu bagaimana caranya YouMaju mencari koneksi untuk anda ?</p>
+              Mau tahu bagaimana YouMaju mengkoneksikan content creator?</p>
           </div>
         </div>
 
-        <div style="color:#343a40; font-size:20px;" >
-                <p align="justify">YouMaju menghubungkan sesama content creator, Support dan berikan feedback anda.
-Majuers harus tahu bahwa dalam menciptakan content, majuers perlu adanya partner atau kerja sama dalam
-menciptakan content yang menarik dan berdampak. YouMaju menghubungkan para content creator untuk
-saling support dengan cara memanfaatkan tools sosial media YouMaju. Jadi majuers jangan ragu untuk
-menggunakan YouMaju karena majuers bisa mendapatkan feedback berupa kemudahan promosi content
-majuers.
+        <div style="color:#343a40; font-size:20px;">
+          <p align="justify">YouMaju menghubungkan sesama content creator, Support dan berikan feedback anda.
+            Majuers harus tahu bahwa dalam menciptakan content, majuers perlu adanya partner atau kerja sama dalam
+            menciptakan content yang menarik dan berdampak. YouMaju menghubungkan para content creator untuk
+            saling support dengan cara memanfaatkan tools sosial media YouMaju. Jadi majuers jangan ragu untuk
+            menggunakan YouMaju karena majuers bisa mendapatkan feedback berupa kemudahan promosi content
+            majuers.
 
-</p>
-            </div>
+          </p>
         </div>
-    </div> 
-    
+      </div>
+    </div>
+
     <footer class="site-footer">
       <div class="container">
         <div class="row">
@@ -69,43 +71,46 @@ majuers.
                 <ul class="list-unstyled">
                   <li><a href="#">Tentang Kami</a></li>
                   <li><a href="#">Kontak Kami</a></li>
-                  </ul>
+                </ul>
               </div>
               <div class="col-md-3">
                 <h2 class="footer-heading mb-4">Majuers ikuti media sosial kami!</h2>
-                 <a href="https://www.instagram.com/youmaju_/" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                            </div>
+                <a href="https://www.instagram.com/youmaju_/" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+              </div>
             </div>
           </div>
           <div class="row col-md-4 text-center">
-          <div class="col-md-12">
-            <div class="border-top">
-            <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy; YouMaju <script>document.write(new Date().getFullYear());</script> 
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
+            <div class="col-md-12">
+              <div class="border-top">
+                <p>
+                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                  Copyright &copy; YouMaju <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+              </div>
             </div>
+
+          </div>
         </div>
-          
-        </div>
-      </div>
     </footer>
-  
 
-  <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
-  <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
-  <script src="{{asset('js/jquery-ui.js')}}"></script>
-  <script src="{{asset('js/popper.min.js')}}"></script>
-  <script src="{{asset('js/bootstrap.min.js')}}"></script>
-  <script src="{{asset('js/owl.carousel.min.js')}}"></script>
-  <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
-  <script src="{{asset('js/jquery.countdown.min.js')}}"></script>
-  <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
-  <script src="{{asset('js/bootstrap-datepicker.min.js')}}"></script>
-  <script src="{{asset('js/aos.js')}}" ></script>
 
-  <script src="{{asset('js/main.js')}}"></script>
-    
-  </body>
+    <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
+    <script src="{{asset('js/jquery-ui.js')}}"></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
+    <script src="{{asset('js/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{asset('js/aos.js')}}"></script>
+
+    <script src="{{asset('js/main.js')}}"></script>
+
+</body>
+
 </html>
--- resources/views/youcreate.blade.php
+++ resources/views/youcreate.blade.php
@@ -1,63 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>YouMaju &mdash; YouCreate</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700"> 
-    <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}">
+<head>
+  <title>YouMaju &mdash; YouCreate</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
-    <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
-    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,700,900|Display+Playfair:200,300,400,700">
+  <link rel="stylesheet" href="{{asset('fonts/icomoon/style.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
+  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/magnific-popup.css')}}">
+  <link rel="stylesheet" href="{{asset('css/jquery-ui.css')}}">
+  <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
 
-    <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
+  <link rel="stylesheet" href="{{asset('css/bootstrap-datepicker.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+  <link rel="stylesheet" href="{{asset('fonts/flaticon/font/flaticon.css')}}">
 
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    
-  </head>
-  <body>
-  
+  <link rel="stylesheet" href="{{asset('css/aos.css')}}">
+
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+</head>
+
+<body>
+
   <div class="site-wrap">
 
     <header class="site-navbar py-3" role="banner">
 
       <div class="container gambaricon">
-        <div class="align-items-center" data-aos="fade-up" data-aos-delay="400" >
-         <a href="#"> <img src="{{asset('images/fav_icon_you_maju_YB.png')}}">
-                  
+        <div class="align-items-center" data-aos="fade-up" data-aos-delay="400">
+          <a href="#"> <img src="{{asset('images/fav_icon_you_maju_YB.png')}}">
+
     </header>
-   
+
     <div class="site-section">
-      <div class="container">   
+      <div class="container">
 
         <div class="row justify-content-center mb-5">
           <div class="col-md-7 text-center border-primary">
             <h2 class="mb-0" style="color:#3c3c32">YouCreate</h2>
             <p class="color-black-opacity-5" style="color:#343a40">
-            Bagaimana tahapan belajar jadi youtuber terkenal ? Disini tempatnya !</p>
+              Mau tahu bagaimana YouMaju mengkoneksikan content creator?</p>
           </div>
         </div>
 
-       <div class="paragraftentang"style="color:#343a40; font-size:20px;" >
-                <p align="justify">YouMaju menciptakan content yang positive, menarik, dan impacfull di YouMaju. 
-Majuers tentunya sudah tidak asing lagi dengan content creator tentunya, tapi apakah majuers tahu bahwa
-menciptakan sebuah content menarik itu ada trik dan tipsnya? majuers harus tahu dahulu bagaimana
-majuers membuat content yang positive, menarik, dan impacfull. YouMaju merupakan salah satu media yang
-membantu majuers dalam menciptakan content yang memberi pengaruh pada lainnya. YouMaju
-memberikan pembelajaran yang bertahap pada tiap sesinya sehingga majuers dapat dengan cepat
-menciptkan content yang memiliki viewer terbanyak pada kelasnya.</p>
-            </div>
+        <div class="paragraftentang" style="color:#343a40; font-size:20px;">
+          <p align="justify">YouMaju menciptakan content yang positive, menarik, dan impacfull di YouMaju.
+            Majuers tentunya sudah tidak asing lagi dengan content creator tentunya, tapi apakah majuers tahu bahwa
+            menciptakan sebuah content menarik itu ada trik dan tipsnya? majuers harus tahu dahulu bagaimana
+            majuers membuat content yang positive, menarik, dan impacfull. YouMaju merupakan salah satu media yang
+            membantu majuers dalam menciptakan content yang memberi pengaruh pada lainnya. YouMaju
+            memberikan pembelajaran yang bertahap pada tiap sesinya sehingga majuers dapat dengan cepat
+            menciptkan content yang memiliki viewer terbanyak pada kelasnya.</p>
         </div>
-    </div> 
-    
+      </div>
+    </div>
+
     <footer class="site-footer">
       <div class="container">
         <div class="row">
@@ -68,43 +70,46 @@ menciptkan content yang memiliki viewer terbanyak pada kelasnya.</p>
                 <ul class="list-unstyled">
                   <li><a href="#">Tentang Kami</a></li>
                   <li><a href="#">Kontak Kami</a></li>
-                  </ul>
+                </ul>
               </div>
               <div class="col-md-3">
                 <h2 class="footer-heading mb-4">Majuers ikuti media sosial kami!</h2>
-                 <a href="https://www.instagram.com/youmaju_/" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
-                            </div>
+                <a href="https://www.instagram.com/youmaju_/" class="pl-3 pr-3"><span class="icon-instagram"></span></a>
+              </div>
             </div>
           </div>
           <div class="row col-md-4 text-center">
-          <div class="col-md-12">
-            <div class="border-top">
-            <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy; YouMaju <script>document.write(new Date().getFullYear());</script> 
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
+            <div class="col-md-12">
+              <div class="border-top">
+                <p>
+                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                  Copyright &copy; YouMaju <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </p>
+              </div>
             </div>
+
+          </div>
         </div>
-          
-        </div>
-      </div>
     </footer>
-  
 
-  <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
-  <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
-  <script src="{{asset('js/jquery-ui.js')}}"></script>
-  <script src="{{asset('js/popper.min.js')}}"></script>
-  <script src="{{asset('js/bootstrap.min.js')}}"></script>
-  <script src="{{asset('js/owl.carousel.min.js')}}"></script>
-  <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
-  <script src="{{asset('js/jquery.countdown.min.js')}}"></script>
-  <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
-  <script src="{{asset('js/bootstrap-datepicker.min.js')}}"></script>
-  <script src="{{asset('js/aos.js')}}" ></script>
 
-  <script src="{{asset('js/main.js')}}"></script>
-    
-  </body>
+    <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('js/jquery-migrate-3.0.1.min.js')}}"></script>
+    <script src="{{asset('js/jquery-ui.js')}}"></script>
+    <script src="{{asset('js/popper.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('js/jquery.stellar.min.js')}}"></script>
+    <script src="{{asset('js/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap-datepicker.min.js')}}"></script>
+    <script src="{{asset('js/aos.js')}}"></script>
+
+    <script src="{{asset('js/main.js')}}"></script>
+
+</body>
+
 </html>
